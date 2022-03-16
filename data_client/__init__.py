98e2d97c44ca9e3a6c9d108cde4a5027f176e464
Jed Zeins <jedzeins@gmail.com>
2022-03-16 19:16:02 +0900

    remove pycache

--- data_client/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils import *
-from bs4 import BeautifulSoup
-
-def get_soup(html):
-    return BeautifulSoup(html,'html.parser')
--- services/util_service/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import dotenv_values
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-dotenv_path = os.path.join(dir_path,'..','..','.env')
-config = dotenv_values(dotenv_path)
-
-def getConfig():
-    return config
